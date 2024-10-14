a180503a77e1faaa58cdb7457d83233da30df9d5
Muhammad Ammar <ch.ammar.tech@gmail.com>
2024-10-14 16:24:09 +0500

    I have created templates file in inventory app , and also defined views.

--- app/inventory/urls.py
+++ app/inventory/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .import views
+from .views import InventoryView
 urlpatterns = [
     
 ]
+#path('inventory/', views.InventoryView.as_view(), name='inventory')
--- app/inventory/views.py
+++ app/inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def InventoryView(View):
+    def get(self, request):
+        return render('inventory.html')
