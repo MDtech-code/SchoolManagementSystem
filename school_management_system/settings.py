a1b70f98031e3158da88fbc0ebe1b19ba398e342
Ammar <ch.ammar.tech@gmail.com>
2024-10-14 15:21:18 +0500

    I have modefied admission app's template. Just chnaged its text

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -104,22 +104,22 @@ WSGI_APPLICATION = 'school_management_system.wsgi.application'
 
 DATABASES = {
     
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     },
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': config('DB_NAME'),
+   #     'USER': config('DB_USER'),
+   #     'PASSWORD': config('DB_PASSWORD'),
+   #     'HOST': config('DB_HOST'),
+   #     'PORT': config('DB_PORT', cast=int),
+   #     'OPTIONS': {
+   #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+   #     }
 
-    }
+   # }
 }
 
 
