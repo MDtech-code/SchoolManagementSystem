e314d8213cb54cb4f4ea1040d019512aacd9db73
Jam364 <jkhudabuksh@gmail.com>
2024-10-15 12:33:58 +0500

    the change in finance app

--- app/finance/urls.py
+++ app/finance/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .import views
+from .views import financeView
 urlpatterns = [
     
 ]
+#path('finance/', views.financeView.as_view(), name='finance')
--- app/finance/views.py
+++ app/finance/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def financeView(View):
+    def get(self, request):
+        return render('finance.html')
