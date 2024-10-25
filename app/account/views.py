4b6393eaf9b9cc6af6519ecdbc017a0f43fde46d
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2024-10-25 04:01:30 -0700

    some signifent change in ui

--- app/account/views.py
+++ app/account/views.py
@@ -264,3 +264,8 @@ class ProfileUpdateView(APIView):
         except Exception as e:
                  return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         '''
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'signup.html')
