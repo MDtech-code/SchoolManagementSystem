13de9acb0c2220f0b2db9052cc20a97d9e6abcbf
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2024-10-30 02:37:53 -0700

    furture more improvement in signup and login desingn

--- app/account/forms.py
+++ app/account/forms.py
@@ -3,14 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserForm(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(label='Password (again)', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Password (again)', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2')
-        widgets = {'username':forms.TextInput(attrs={'class':'form-control'}), 
-                    'email':forms.EmailInput(attrs={'class':'form-control'})}
-
 # forms.py
 
 
